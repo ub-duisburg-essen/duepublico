eba096c0c78c191bb787eb31b1851e1b410eddea
Paul Rochowski <Paul.Rochowski@uni-due.de>
2018-01-31 10:10:48 +0000

    # working on statistics ui representation

--- duepublico-webapp/src/main/webapp/js/duepublico/duepublico.main.js
+++ duepublico-webapp/src/main/webapp/js/duepublico/duepublico.main.js
@@ -1,20 +1,41 @@
 /**
  * @author Paul Rochowski
  */
-$(document).ready(function() {
+$(document).ready(function () {
 
-	console.log("bind statistics");
-//	
-//	/*
-//	 * add javascript extension by contextpath 
-//	 */
-//	var generatedurl = webApplicationBaseURL;
-//	
-//	/*
-//	 * event handler on actionbox
-//	 */
-//	$('div.btn btn-default btn-sm dropdown-toggle').click(function (e) {
-//		
-//	}
+    if (this.URL.startsWith(webApplicationBaseURL + 'receive/')) {
+
+        console.log("bind statistics");
+
+        var $systemPanel = $("#main_content #aux_col");
+
+        let templateStatistics = `
+        
+<div class="panel panel-default" id="duepublico_statistics">
+
+    <div class="panel-heading">
+        <h3 class="panel-title">Zugriffsstatistik</h3>
+    </div>
+    <div class="panel-body">
+        <div class="form-inline controls">
+            <select class="form-control form-control-inline">
+                <option title="Januar" value="ar">Januar</option>
+                <option title="Februar" value="bs">Februar</option>
+                <option title="März" value="bg">März</option>
+                <option title="April" value="zh">April</option>
+                <option title="Mai" value="da">Mai</option>
+                <option title="Juni" value="en">Juni</option>
+                <option title="Juli" value="fi">Juli</option>
+                <option title="August" value="fr">August</option>
+                <option title="September" value="el">September</option>
+                <option title="Oktober" value="heb">Oktober</option>
+                <option title="November" value="nl">November</option>
+                <option title="Dezember" value="id">Dezember</option>
+            </select>
+        </div>
+    </div>
+</div>`;
+
+        $($systemPanel).append(templateStatistics);
+    }
 });
-
