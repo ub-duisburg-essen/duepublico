017c8f5a37ee10ee87ad6d14182869fe21aba9f4
Frank Lützenkirchen <frank.luetzenkirchen@uni-due.de>
2020-09-28 13:18:41 +0200

    Fixed mods:note editing, postprocessing and migration command

---
+++ src/main/java/unidue/ub/duepublico/DuEPublicoPostProcessor.java
@@ -0,0 +1,47 @@
+package unidue.ub.duepublico;
+
+import java.io.IOException;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.filter.Filters;
+import org.jdom2.xpath.XPathExpression;
+import org.jdom2.xpath.XPathFactory;
+import org.mycore.common.MCRConstants;
+import org.mycore.mir.editor.MIREditorUtils;
+import org.mycore.mir.editor.MIRPostProcessor;
+import org.xml.sax.SAXException;
+
+public class DuEPublicoPostProcessor extends MIRPostProcessor {
+
+    private static final String XPATH_NOTE = "//mods:mods/mods:note[string-length(text()) > 0]";
+
+    private XPathExpression<Element> nxp;
+
+    public DuEPublicoPostProcessor() {
+        super();
+
+        nxp = XPathFactory.instance().compile(XPATH_NOTE, Filters.element(), null, MCRConstants.MODS_NAMESPACE);
+    }
+
+    @Override
+    public Document process(Document oldXML) throws IOException, JDOMException, SAXException {
+        final Document newXML = oldXML.clone();
+
+        for (Element note : nxp.evaluate(newXML)) {
+            String textBefore = note.getText();
+
+            if (!(textBefore.contains("<") || textBefore.contains("&")))
+                continue; // does not smell like html
+
+            String textAfter = MIREditorUtils.getXHTMLSnippedString(textBefore);
+            if (textBefore.equals(textAfter))
+                continue; // no change
+            else
+                note.setText(textAfter);
+        }
+
+        return super.process(newXML);
+    }
+}
--- src/main/java/unidue/ub/duepublico/NoteHTMLMigration.java
+++ src/main/java/unidue/ub/duepublico/NoteHTMLMigration.java
@@ -1,35 +1,17 @@
 package unidue.ub.duepublico;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.StringReader;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.Objects;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
-import org.jdom2.Document;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jdom2.Element;
-import org.jdom2.input.SAXBuilder;
-import org.jdom2.output.DOMOutputter;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Entities;
-import org.mycore.access.MCRAccessException;
 import org.mycore.common.MCRConstants;
-import org.mycore.common.MCRPersistenceException;
 import org.mycore.datamodel.common.MCRXMLMetadataManager;
 import org.mycore.datamodel.metadata.MCRMetadataManager;
 import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.datamodel.metadata.MCRObjectID;
-import org.mycore.frontend.cli.MCRObjectCommands;
 import org.mycore.frontend.cli.annotation.MCRCommand;
 import org.mycore.frontend.cli.annotation.MCRCommandGroup;
+import org.mycore.mir.editor.MIREditorUtils;
 import org.mycore.mods.MCRMODSWrapper;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @MCRCommandGroup(name = "DuEPublico migration 2020.06")
 public class NoteHTMLMigration {
@@ -57,18 +39,21 @@ public class NoteHTMLMigration {
         boolean fixed = false;
         for (Element note : mods.getChildren("note", MCRConstants.MODS_NAMESPACE)) {
             String textBefore = note.getText();
-            if (!textBefore.contains("<"))
+
+            if (!(textBefore.contains("<") || textBefore.contains("&")))
                 continue; // does not smell like html
 
-            String textAfter = convert(textBefore);
+            String textAfter = MIREditorUtils.getXHTMLSnippedString(textBefore);
             if (textBefore.equals(textAfter))
                 continue; // no change
 
+            System.out.println("--------------------------------");
+            System.out.println("before: " + textBefore);
+            System.out.println(" after: " + textAfter);
+            System.out.println("--------------------------------");
+
             if (textAfter == null) { // conversion did not work
-                System.out.println("Can't convert " + oid);
-                System.out.println("--------------------------------");
-                System.out.println("before: " + textBefore);
-                System.out.println(" after: " + textAfter);
+                System.out.println("WARNING: Can't convert " + oid);
                 System.out.println("--------------------------------");
                 continue;
             }
@@ -84,23 +69,5 @@ public class NoteHTMLMigration {
             } catch (Exception ex) {
                 LOGGER.warn(ex);
             }
-    }
-
-    private static String convert(String text) {
-        try {
-            byte[] data = text.getBytes(StandardCharsets.UTF_8);
-            final org.jsoup.nodes.Document jsoupDoc = Jsoup.parse(new ByteArrayInputStream(data), null, "");
-            jsoupDoc.outputSettings().prettyPrint(false);
-            jsoupDoc.outputSettings().escapeMode(Entities.EscapeMode.xhtml);
-            jsoupDoc.outputSettings().syntax(org.jsoup.nodes.Document.OutputSettings.Syntax.xml);
-            String tmp = jsoupDoc.body().html();
-
-            String test = "<test>" + tmp + "</test>";
-            new SAXBuilder().build(new ByteArrayInputStream(test.getBytes(StandardCharsets.UTF_8)));
-
-            return tmp;
-        } catch (Exception ex) {
-            return null;
-        }
     }
 }
