10f76630b1a1c9784a22e97387f872a12b7eb15d
adg167c <adg167c@UB-Digibib-060M.win.uni-due.de>
2022-06-02 13:44:38 +0200

    Redmine/issues/4852 - reproduce java.lang.OutOfMemoryError

---
+++ src/main/java/unidue/ub/duepublico/HeapSpaceTestServlet.java
@@ -0,0 +1,33 @@
+package unidue.ub.duepublico;
+
+import java.util.concurrent.TimeUnit;
+import javax.servlet.annotation.WebServlet;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.mycore.frontend.servlets.MCRServlet;
+import org.mycore.frontend.servlets.MCRServletJob;
+
+@WebServlet(name = "HeapSpaceTestServlet", urlPatterns = { "/servlets/HeapSpaceTestServlet" })
+public class HeapSpaceTestServlet extends MCRServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	protected void doGetPost(MCRServletJob job) throws Exception {
+
+		LOGGER.info("Reproducing java heap space error");
+
+		while (true) {
+			new Thread(() -> {
+				try {
+					TimeUnit.HOURS.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}).start();
+		}
+	}
+
+}
