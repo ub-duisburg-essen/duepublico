7dd47f5462823f2e75044e12345259840deee030
adg167c <paul.rochowski@uni-due.de>
2024-04-22 13:50:09 +0200

    adapt sandbox

---
+++ dockerize/duepublico-eclipse/dockerize_duepublico_eclipse.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+timestamp=$(date)
+
+logtemplate=""$timestamp" - dockerize_duepublico_eclipse.sh:"
+
+# is docker running ?
+if ! docker info >/dev/null 2>&1; then
+
+	printf '%s This script uses docker, and it is not running or you does not have permission to use it - please start docker and try again!\n' "$logtemplate"
+	exit 1
+fi
+
+docker build -f dockerfile -t duepublico-eclipse:master . >/dev/null
+sudo docker container run --rm --net=host -it --env=DISPLAY --volume=$HOME/.Xauthority:/root/.Xauthority:rw --name duepublico-eclipse_master duepublico-eclipse:master
+
+# check if mycore home exists
+#if sudo docker exec -it duepublico-eclipse_master sh -c "test  -d /home/mycore/.mycore/duepublico"
+
+exit 0
