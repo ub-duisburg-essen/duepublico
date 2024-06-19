df9bb9fd2f75b947fefc28da25d3fe7ac170f54b
Paul Rochowski <paul.rochowski@uni-due.de>
2024-06-19 08:27:57 +0200

    redmine/issues/56507_provide_data_export add import script

--- duepublico-tools/duepublico_clean_import_data.sh
+++ duepublico-tools/duepublico_clean_import_data.sh
@@ -3,15 +3,20 @@ logtemplate=" - duepublico_clean_import_data.sh:"
 
 while getopts d:u: flag; do
 	case "${flag}" in
-	d) data_directory=${OPTARG} ;;
+	d) mcr_home=${OPTARG} ;;
 	u) data_url=${OPTARG} ;;
 	esac
 done
 
 # flags d, u are required
-if [[ -z "$data_directory" || -z "$data_url" ]]; then
+if [[ -z "$mcr_home" || -z "$data_url" ]]; then
 
-	printf '%s This script requires flags -d (mcr data directory) and -u (data url with encrypted mcr data archive) -> please make them available\n' "$(date) $logtemplate"
+	printf '%s This script requires flags -d (mcr home directory) and -u (data url with encrypted mcr data archive) -> please make them available\n' "$(date) $logtemplate"
+	exit 1
+fi
+
+if ! [ -d $mcr_home/data ]; then
+	printf '%s Error - Invalid mcr home directory \n' "$(date) $logtemplate"
 	exit 1
 fi
 
@@ -43,6 +48,32 @@ openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -iter 1000000 -salt -in ./env/tmp
 printf "%s Unpack mcr data archive\n" "$(date) $logtemplate"
 tar xfz ./env/tmp/data.tar.gz -C ./env/tmp
 
-printf '%s mcr data archive was recovered successfully\n' "$(date) $logtemplate"
+#check data archive
+if ! [ -d ./env/tmp/data ] ||
+	! [ -d ./env/tmp/data/content ] || ! [ -d ./env/tmp/data/metadata ] || ! [ -d ./env/tmp/data/versions-metadata ]; then
+
+	printf "%s Error - Downloaded archive from $data_url hasn't got the required structure \n" "$(date) $logtemplate"
+	exit 1
+fi
+
+printf "%s Clean current mcr data directory in $mcr_home \n" "$(date) $logtemplate"
+if [ -d $mcr_home/data/metadata ]; then
+	printf "%s Remove $mcr_home/data/metadata\n" "$(date) $logtemplate"
+	rm -rf $mcr_home/data/metadata
+fi
+
+if [ -d $mcr_home/data/versions-metadata ]; then
+	printf "%s Remove $mcr_home/data/versions-metadata\n" "$(date) $logtemplate"
+	rm -rf $mcr_home/data/versions-metadata
+fi
+
+if [ -d $mcr_home/data/content ]; then
+	printf "%s Remove $mcr_home/data/content\n" "$(date) $logtemplate"
+	rm -rf $mcr_home/data/content
+fi
+
+printf "%s Move\n" "$(date) $logtemplate"
+
+printf '%s MCR data archive was recovered successfully\n' "$(date) $logtemplate"
 
 exit 0
