c51a38e6d8b7928f2bb443d00f4087b817c1d907
Eva Roddeck <eva.roddeck@uni-due.de>
2024-06-26 13:09:04 +0200

    delete unused import

--- duepublico-extras/src/main/java/unidue/ub/duepublico/XFDF2PDFTransformer.java
+++ duepublico-extras/src/main/java/unidue/ub/duepublico/XFDF2PDFTransformer.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import javax.xml.transform.TransformerException;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.pdfbox.pdmodel.PDDocument;
