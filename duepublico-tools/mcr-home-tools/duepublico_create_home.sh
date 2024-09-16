cc80493f8a1ef1e0c82bdfc07bbc4632490ab56d
Paul Rochowski <paul.rochowski@uni-due.de>
2024-09-16 07:47:04 +0200

    adapt duepublico mchome creation script

--- duepublico-tools/mcr-home-tools/duepublico_create_home.sh
+++ duepublico-tools/mcr-home-tools/duepublico_create_home.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-logtemplate=" - duepublico_wizard.sh:"
+logtemplate=" - duepublico_create_home.sh:"
 current_dir=$(pwd)
 
 appname="duepublico"
@@ -7,8 +7,15 @@ appname="duepublico"
 h2_user="sa"
 h2_password=""
 
-cd ..
+cd ../..
 
+# check for current mycore home and clean
+if [ -d ~/.mycore/$appname ]; then
+	printf "%s Remove previous mycore home\n" "$(date) $logtemplate"
+	rm -rf ~/.mycore/$appname
+fi
+
+# Stop server (if running)
 printf "%s Check running solr and webapp\n" "$(date) $logtemplate"
 if [ $(
 	nc -z -w30 localhost 8983
@@ -35,9 +42,6 @@ fi
 printf "%s Clean and (re)install duepublico2\n" "$(date) $logtemplate"
 git submodule init && git submodule update
 mvn clean install -DskipTests
-
-printf "%s Remove current mycore home\n" "$(date) $logtemplate"
-rm -rf ~/.mycore/$appname
 
 cd duepublico-setup
 
@@ -73,10 +77,10 @@ if ! [ -d ~/.mycore/$appname/resources ]; then
 	cp -rp ./resources ~/.mycore/$appname
 
 	# Adapt persistence.xml
-	printf "%s Adapt persistence.xml in ~/.mycore\/$appname\/resources\/META-INF\/persistence.xml\n" "$(date) $logtemplate"
-	sed -i "s/javax.persistence.jdbc.url\" value=\"/javax.persistence.jdbc.url\" value=\"jdbc:h2:~\/.mycore\/$appname\/resources\/META-INF\/persistence.xml/g" ~/.mycore/$appname/resources/META-INF/persistence.xml
-	sed -i "s/javax.persistence.jdbc.user\" value=\"/javax.persistence.jdbc.user\" value=\"$h2_user/g" ~/.mycore/$appname/resources/META-INF/persistence.xml
-	sed -i "s/javax.persistence.jdbc.password\" value=\"/javax.persistence.jdbc.password\" value=\"$h2_password/g" ~/.mycore/$appname/resources/META-INF/persistence.xml
+	#printf "%s Adapt persistence.xml in ~/.mycore\/$appname\/resources\/META-INF\/persistence.xml\n" "$(date) $logtemplate"
+	#sed -i "s/javax.persistence.jdbc.url\" value=\"/javax.persistence.jdbc.url\" value=\"jdbc:h2:~\/.mycore\/$appname\/resources\/META-INF\/persistence.xml/g" ~/.mycore/$appname/resources/META-INF/persistence.xml
+	#sed -i "s/javax.persistence.jdbc.user\" value=\"/javax.persistence.jdbc.user\" value=\"$h2_user/g" ~/.mycore/$appname/resources/META-INF/persistence.xml
+	#sed -i "s/javax.persistence.jdbc.password\" value=\"/javax.persistence.jdbc.password\" value=\"$h2_password/g" ~/.mycore/$appname/resources/META-INF/persistence.xml
 
 	# Adapt solr settings
 	printf "%s Add solr settings into ~/.mycore/$appname/mycore.properties\n" "$(date) $logtemplate"
@@ -86,7 +90,7 @@ if ! [ -d ~/.mycore/$appname/resources ]; then
 	echo "MCR.Solr.ServerURL=http\://localhost\:8983" >>~/.mycore/$appname/mycore.properties
 
 	# Use CLI
-	cd .. && cd duepublico-setup
+	cd ../.. && cd duepublico-setup
 
 	# Reload solr configuration
 	./target/bin/duepublico.sh reload solr configuration main in core main
@@ -94,17 +98,16 @@ if ! [ -d ~/.mycore/$appname/resources ]; then
 
 	# Load classifications
 	./target/bin/duepublico.sh update all classifications from directory ./src/main/setup/classifications
-
+	
 	# Init superuser
 	./target/bin/duepublico.sh init superuser
 
 	# Import permissions
 	./target/bin/duepublico.sh load permissions data from file ./src/main/setup/permissions.xml
-
+	
 	# ediss user
 	./target/bin/duepublico.sh import user from file ./src/main/setup/user-ediss.xml
 	./target/bin/duepublico.sh set password for user ediss to ediss
-
 fi
 
 printf "%s duepublico2 home was created successfully.\n" "$(date) $logtemplate"
