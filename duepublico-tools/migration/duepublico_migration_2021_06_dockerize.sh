829ee37d370ac96e72ed9a0ac737aa5c30f9029c
Paul Rochowski <paul.rochowski@uni-due.de>
2024-12-13 13:15:01 +0100

    Create tools for migration 2021.06

--- duepublico-tools/migration/duepublico_migration_2021_06_dockerize.sh
+++ duepublico-tools/migration/duepublico_migration_2021_06_dockerize.sh
@@ -23,8 +23,38 @@ if ! [ -f ../../duepublico-webapp/target/duepublico-webapp-2.0-SNAPSHOT.war ]; t
     exit 1
 fi
 
+# create temp
+if [ -d ./temp ]; then
+    rm -rf ./temp
+fi
+
+printf "%s Get current mycore solr configset from github\n" "$(date) $logtemplate"
+mkdir ./temp && mkdir ./temp/configsets && cd ./temp/configsets
+# Get current mycore configsets from github
+git clone https://github.com/MyCoRe-Org/mycore_solr_configset_classification >/dev/null 2>&1
+git clone https://github.com/MyCoRe-Org/mycore_solr_configset_main >/dev/null 2>&1
+cd ../..
+
 printf '%s Cleanup dependent docker containersp\n' "$logtemplate"
 # check if there are running containers and cleanup
+if $(docker inspect -f '{{.State.Running}}' duepublico-war) = "true"; then
+    # stop current containers
+    printf '%s Stop current docker container duepublico-war\n' "$(date) $logtemplate"
+    docker stop duepublico-war >/dev/null
+
+    printf '%s Remove current docker container duepublico-war\n' "$(date) $logtemplate"
+    docker rm duepublico-war >/dev/null
+fi
+
+if $(docker inspect -f '{{.State.Running}}' duepublico-solr) = "true"; then
+    # stop current containers
+    printf '%s Stop current docker container duepublico-solr\n' "$(date) $logtemplate"
+    docker stop duepublico-solr >/dev/null
+
+    printf '%s Remove current docker container duepublico-solr\n' "$(date) $logtemplate"
+    docker rm duepublico-solr >/dev/null
+fi
+
 if $(docker inspect -f '{{.State.Running}}' duepublico-postgres) = "true"; then
     # stop current containers
     printf '%s Stop current docker container duepublico-postgres\n' "$(date) $logtemplate"
@@ -34,14 +64,9 @@ if $(docker inspect -f '{{.State.Running}}' duepublico-postgres) = "true"; then
     docker rm duepublico-postgres >/dev/null
 fi
 
-if $(docker inspect -f '{{.State.Running}}' duepublico-war) = "true"; then
-    # stop current containers
-    printf '%s Stop current docker container duepublico-war\n' "$(date) $logtemplate"
-    docker stop duepublico-war >/dev/null
-
-    printf '%s Remove current docker container duepublico-war\n' "$(date) $logtemplate"
-    docker rm duepublico-war >/dev/null
-fi
+printf '%s Remove dependent docker images to provide clean build\n' "$logtemplate"
+docker rmi duepublico-war:latest >/dev/null
+docker rmi duepublico-solr:latest >/dev/null
 
 printf '%s Build duepublico-solr image\n' "$logtemplate"
 docker build -f ../solr/dockerfile -t duepublico-solr:latest . >/dev/null
@@ -52,6 +77,12 @@ docker build -t duepublico-war:latest . >/dev/null
 printf '%s Start docker containers with docker compose\n' "$logtemplate"
 docker compose up -d
 
-printf '%s duepublico (migration 2021.06) was created successfully in docker container\n' "$(date) $logtemplate"
+# Wait for solr and db
+sleep 10
 
+printf '%s Create cores from mycore configsets image\n' "$(date) $logtemplate"
+docker exec -it duepublico-solr solr create -c duepublico-classifications -d /var/solr/temp/configsets/mycore_solr_configset_classification
+docker exec -it duepublico-solr solr create -c duepublico -d /var/solr/temp/configsets/mycore_solr_configset_main
+
+printf '%s duepublico (migration 2021.06) was created successfully in docker container\n' "$(date) $logtemplate"
 exit 0
