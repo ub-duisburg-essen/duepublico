d919a7489a6a03744e22454e21331c14a77d1bba
adg167c <paul.rochowski@uni-due.de>
2026-03-30 11:23:49 +0200

    automate cert import for dockerized duepublico 2023

--- duepublico-tools/2023.06/duepublico_2023_dockerize.sh
+++ duepublico-tools/2023.06/duepublico_2023_dockerize.sh
@@ -6,52 +6,59 @@ mcr_export="./env/mcr_export/"
 sql_dump=$mcr_export"db/duepublico_2023_migration.sql"
 
 function prop {
-    grep "${1}" ./.env | cut -d "=" -f2
+  grep "${1}" ./.env | cut -d "=" -f2
 }
 
 # is docker running ?
 if ! docker info >/dev/null 2>&1; then
 
-    printf '%s This script uses docker, and it is not running or you does not have permission to use it - please start docker and try again!\n' "$(date) $logtemplate"
-    exit 1
+  printf '%s This script uses docker, and it is not running or you does not have permission to use it - please start docker and try again!\n' "$(date) $logtemplate"
+  exit 1
 fi
 
 # Are there needed environmental files ?
 if ! [ -f ./.env ]; then
 
-    printf '%s This script needs environmental information (.env file) - please make them available!\n' "$(date) $logtemplate"
-    exit 1
+  printf '%s This script needs environmental information (.env file) - please make them available!\n' "$(date) $logtemplate"
+  exit 1
 fi
 
 # Is there needed maven build ?
 if ! [ -f ../../duepublico-webapp/target/duepublico-webapp-2023.06.5-SNAPSHOT.war ]; then
 
-    printf '%s This script needs a maven build of duepublico - please make them available!\n' "$(date) $logtemplate"
-    exit 1
+  printf '%s This script needs a maven build of duepublico - please make them available!\n' "$(date) $logtemplate"
+  exit 1
+fi
+
+# Is there needed certificate ?
+if ! [ -f ./env/cert/HARICA-TLS-Root-2021-RSA.pem ]; then
+
+  printf '%s This script needs a maven build of duepublico - please make them available!\n' "$(date) $logtemplate"
+  exit 1
 fi
 
 printf '%s Cleanup dependent docker containers\n' "$logtemplate"
 # check if there are running containers and cleanup
 if $(docker inspect -f '{{.State.Running}}' duepublico-2023-war) = "true"; then
-    # stop current containers
-    printf '%s Stop current docker container duepublico-2023-war\n' "$(date) $logtemplate"
-    docker stop duepublico-2023-war >/dev/null
+  # stop current containers
+  printf '%s Stop current docker container duepublico-2023-war\n' "$(date) $logtemplate"
+  docker stop duepublico-2023-war >/dev/null
 fi
 printf '%s Remove current docker container duepublico-2023-war\n' "$(date) $logtemplate"
 docker rm duepublico-2023-war >/dev/null
 
 if $(docker inspect -f '{{.State.Running}}' duepublico-2023-solr) = "true"; then
-    # stop current containers
-    printf '%s Stop current docker container duepublico-2023-solr\n' "$(date) $logtemplate"
-    docker stop duepublico-2023-solr >/dev/null
+  # stop current containers
+  printf '%s Stop current docker container duepublico-2023-solr\n' "$(date) $logtemplate"
+  docker stop duepublico-2023-solr >/dev/null
 fi
 printf '%s Remove current docker container duepublico-2023-solr\n' "$(date) $logtemplate"
 docker rm duepublico-2023-solr >/dev/null
 
 if $(docker inspect -f '{{.State.Running}}' duepublico-2023-postgres) = "true"; then
-    # stop current containers
-    printf '%s Stop current docker container duepublico-2023-postgres\n' "$(date) $logtemplate"
-    docker stop duepublico-2023-postgres >/dev/null
+  # stop current containers
+  printf '%s Stop current docker container duepublico-2023-postgres\n' "$(date) $logtemplate"
+  docker stop duepublico-2023-postgres >/dev/null
 fi
 printf '%s Remove current docker container duepublico-2023-postgres\n' "$(date) $logtemplate"
 docker rm duepublico-2023-postgres >/dev/null
@@ -64,25 +71,25 @@ docker rmi postgres:17-bookworm >/dev/null
 # check if there is a solr volume
 if [ ! -d ./env/duepublico-2023-solr ]; then
 
-    printf '%s There is no existing solr volume. \n' "$(date) $logtemplate"
-    mkdir ./env/duepublico-2023-solr
+  printf '%s There is no existing solr volume. \n' "$(date) $logtemplate"
+  mkdir ./env/duepublico-2023-solr
 
-    printf '%s Create cores from mycore configsets image\n' "$(date) $logtemplate"
+  printf '%s Create cores from mycore configsets image\n' "$(date) $logtemplate"
 
-    printf '%s Check if configset is available locally (necessary for offline build)\n' "$(date) $logtemplate"
-    # create temp
-    if [ ! -d ./env/temp ]; then
-        printf "%s Get current mycore solr configset from github\n" "$(date) $logtemplate"
-        mkdir ./env/temp && mkdir ./env/temp/configsets && cd ./env/temp/configsets
-        # Get current mycore configsets from github
-        git clone https://github.com/MyCoRe-Org/mycore_solr_configset_classification >/dev/null 2>&1
-        git clone https://github.com/MyCoRe-Org/mycore_solr_configset_main >/dev/null 2>&1
-        cd ../../..
-    fi
-    CREATE_CORES=true
+  printf '%s Check if configset is available locally (necessary for offline build)\n' "$(date) $logtemplate"
+  # create temp
+  if [ ! -d ./env/temp ]; then
+    printf "%s Get current mycore solr configset from github\n" "$(date) $logtemplate"
+    mkdir ./env/temp && mkdir ./env/temp/configsets && cd ./env/temp/configsets
+    # Get current mycore configsets from github
+    git clone https://github.com/MyCoRe-Org/mycore_solr_configset_classification >/dev/null 2>&1
+    git clone https://github.com/MyCoRe-Org/mycore_solr_configset_main >/dev/null 2>&1
+    cd ../../..
+  fi
+  CREATE_CORES=true
 
 else
-    printf '%s solr data detected in ./env/duepublico-2023-solr\n' "$(date) $logtemplate"
+  printf '%s solr data detected in ./env/duepublico-2023-solr\n' "$(date) $logtemplate"
 fi
 
 printf '%s Build duepublico-2023-solr image\n' "$(date) $logtemplate"
@@ -98,13 +105,33 @@ docker-compose up -d
 sleep 10
 
 if [ "$CREATE_CORES" = true ]; then
-    printf '%s Create cores from mycore configsets \n' "$(date) $logtemplate"
-    docker exec -it duepublico-2023-solr solr create -c duepublico_classifications -d /var/temp/configsets/mycore_solr_configset_classification
-    docker exec -it duepublico-2023-solr solr create -c duepublico_main -d /var/temp/configsets/mycore_solr_configset_main
+  printf '%s Create cores from mycore configsets \n' "$(date) $logtemplate"
+  docker exec -it duepublico-2023-solr solr create -c duepublico_classifications -d /var/temp/configsets/mycore_solr_configset_classification
+  docker exec -it duepublico-2023-solr solr create -c duepublico_main -d /var/temp/configsets/mycore_solr_configset_main
 fi
 
 printf '%s Restore sql_dump into duepublico-2023-postgres container\n' "$(date) $logtemplate"
 cat $sql_dump | docker exec -i duepublico-2023-postgres psql -d $(prop POSTGRES_DB) -U $(prop POSTGRES_USER)
 
+printf '%s Import cert with keytool \n' "$(date) $logtemplate"
+
+# Execute the keytool import command
+docker exec duepublico-2023-war keytool -import \
+  -trustcacerts \
+  -noprompt \
+  -file /root/cert/HARICA-TLS-Root-2021-RSA.pem \
+  -keystore /usr/local/openjdk-17/lib/security/cacerts \
+  -storepass $(prop STORE_PASS)
+
+# Check the exit status of the previous command ($?)
+if [ $? -eq 0 ]; then
+  printf '%s Success: Certificate imported. Restarting container duepublico-2023-war \n' "$(date) $logtemplate"
+  docker restart duepublico-2023-war
+else
+  printf '%s Error: Certificate import failed! \n' "$(date) $logtemplate"
+  exit 1
+fi
+
 printf '%s duepublico (migration 2023.06) was created successfully in docker container\n' "$(date) $logtemplate"
+
 exit 0
